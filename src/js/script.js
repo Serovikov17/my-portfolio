c6085e10d5661e3e05a6a581ea6a65e2e0c120a9
serovikov_kirill@mail.ru <K17ser96>
2021-03-05 16:21:27 +0500

    4.15_script

--- src/js/script.js
+++ src/js/script.js
@@ -10,4 +10,11 @@ document.addEventListener("DOMContentLoaded", () => {
     closeElem.addEventListener('click', () => {
     menu.classList.remove('active');
     });
+
+    const counters = document.querySelectorAll('.skills__ratings-counter'),
+          lines = document.querySelectorAll('.skills__ratings-line span');
+
+    counters.forEach( (item, i) => {
+        lines[i].style.width = item.innerHTML;
+    });
 });
