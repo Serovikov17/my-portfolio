c96ae2efb129435eab36c3eafd70bd08a2d56480
serovikov_kirill@mail.ru <K17ser96>
2021-03-12 15:26:58 +0500

    adaptation

--- dist/js/script.js
+++ dist/js/script.js
@@ -17,4 +17,22 @@ document.addEventListener("DOMContentLoaded", () => {
     counters.forEach( (item, i) => {
         lines[i].style.width = item.innerHTML;
     });
+
+    
 });
+
+/* $('form').submit(function(e) {
+    e.preventDefault();
+    $.ajax({
+        type: "POST",
+        url: "mailer/smart.php",
+        data: $(this).serialize()
+    }).done(function() {
+        $(this).find("input").val("");
+        $('#consultation, #order').fadeOut();
+        $('.overlay, #thanks').fadeIn('slow');
+
+        $('form').trigger('reset');
+    });
+    return false;
+}); */
